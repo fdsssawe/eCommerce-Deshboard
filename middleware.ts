63de5da9fea9fa78fcd706413204a767f0cb4446
Olexander Zhovanyk <mindesync@gmail.com>
2023-07-25 23:47:52 +0300

    Middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  ignoredRoutes: ["/api/:path*"],
+  publicRoutes: ["/api/:path*"],
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};  
+};
