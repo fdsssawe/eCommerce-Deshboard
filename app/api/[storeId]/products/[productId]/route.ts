ed2396dc733f4d79df840fe4359526726c45fcf8
Olexander Zhovanyk <mindesync@gmail.com>
2023-07-27 23:47:34 +0300

    API routes fixed - Endpoint for getting specific category fixed to provide billboard - Endpoint for updating product image check fixed

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -54,7 +54,7 @@ export async function PATCH(
             return new NextResponse("Name is required" , {status : 400})
         }
 
-        if(!images || !images.lenght){
+        if(!images[0]){
             return new NextResponse("Images is required" , {status : 400})
         }
 
